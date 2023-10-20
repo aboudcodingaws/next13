18cc4dd9ba30c86cde798051c3e58ceb1368ced7
Abdelqader Al-Omari <aboud.coding@gmail.com>
2023-10-20 23:35:55 +0300

    Create functionality added

--- components/TopicsList.tsx
+++ components/TopicsList.tsx
@@ -2,21 +2,39 @@ import Link from "next/link";
 import RemoveBtn from "./RemoveBtn";
 import {HiPencilAlt} from "react-icons/hi";
 
-export default function TopicsList(){
+const getTopics = async () =>{
+    try {
+        const res = await fetch("http://localhost:3001/api/topics",{
+            cache: "no-store",
+        });
+        if(!res.ok){
+            throw new Error("Failed to fetch topics");
+        }
+        return res.json();
+    } catch (error) {
+        console.log("Error loading topics", error);
+    }
+}
+
+export default async function TopicsList(){
+    const {topics} = await getTopics() || {};
     return (
         <>
+            {topics.map((topic: any) => (
+            // eslint-disable-next-line react/jsx-key
             <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
                 <div>
-                    <h2 className="font-bold text-2xl">Topic Title</h2>
-                    <div>Topic Description</div>
+                    <h2 className="font-bold text-2xl">{topic.title}</h2>
+                    <div>{topic.description}</div>
                 </div>
                 <div className="flex gap-2">
                     <RemoveBtn/>
-                    <Link href={'/editTopic/123'}>
+                    <Link href={`/editTopic/${topic._id}`}>
                         <HiPencilAlt size={24} />
                     </Link>
                 </div>
             </div>
+            ))}
         </>
     )
 }
